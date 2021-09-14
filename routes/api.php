08966fcbb8b9a8f9005c410935dd6a347045d3aa
Ernesto Maya <emaya167@gmail.com>
2021-09-14 17:54:31 -0500

    validacion de acceso ruta OE

--- routes/api.php
+++ routes/api.php
@@ -152,7 +152,7 @@ Route::group(['middleware' => 'auth:api'], function () {
     //list delivery orders 
     Route::get('orden-de-entrega/listar', 'deliveryOrdersController@deliveryOrdersList')->middleware('AccessOE');
     // delivery orders register resoucres
-    Route::get('orden-de-entrega/registrar-oe-recursos', 'deliveryOrdersController@registerDeliveryOrderResources');
+    Route::get('orden-de-entrega/registrar-oe-recursos', 'deliveryOrdersController@registerDeliveryOrderResources')->middleware('AccessOE');
     //delivery orders get inks desing
     Route::get('orden-de-entrega/diseno-tintas', 'deliveryOrdersController@getInkDesing')->middleware('AccessOE');
     //delivery orders create
